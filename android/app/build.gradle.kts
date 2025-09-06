91aef2aa52c8a56829f2088d0bef63e26512c57f
Meet Janani <meet@backbase.com>
2025-09-06 18:30:41 +0530

    Update version code to 4 and switch payment gateway key to live environment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
     }
 
