41eec7163b69ba07855a37e2940deb1f0ebd3d54
Meet Janani <meetjanani47@gmail.com>
2025-11-13 19:33:10 +0530

    version upgrade from 4 to 5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0.0"
     }
 
