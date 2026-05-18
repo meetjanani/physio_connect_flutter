02b6ca32a8915ddf8be35f5e33f40e78abd071f4
Meet Janani <meet+backbase@backbase.com>
2026-05-18 22:50:38 +0530

    version upgrade from 5 to 6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 5 // 6
+        versionCode = 6
         versionName = "1.0.0"
     }
 
